d6c7247931df266ab41346b39685164d3bcfc85b
mlkycow <vileninuljanov@yandex.ru>
2024-01-28 18:01:05 +0300

    Fixing missing symbols deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "Deploying & installing reddit-monolith"
 sudo apt install -y git
 cd /home/yc-user
 git clone -b monolith https://github.com/express42/reddit.git
-cd /home/yc-user/redit
+cd /home/yc-user/reddit
 bundle install
 puma -d
 ps aux | grep puma
