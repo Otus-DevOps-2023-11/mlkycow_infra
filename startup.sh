4602d7ac0c6764f9016dfa21f60398f678a66690
mlkycow <vileninuljanov@yandex.ru>
2024-01-28 17:54:45 +0300

    Fixing startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ sudo apt install mongodb -y
 
 cd /home/yc-user
 git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
+cd /home/yc-user/reddit && bundle install
 puma -d
 
 ps aux | grep puma
