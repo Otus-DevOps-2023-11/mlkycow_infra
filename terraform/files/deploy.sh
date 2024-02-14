2bf7318ff3ce331a5d8e13b092c6ff65ad4590f6
mlkycow <vileninuljanov@yandex.ru>
2024-02-15 02:19:55 +0300

    terraform-1 job is done without stars

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo rm -f /var/lib/apt/lists/lock
+sudo rm -f /var/cache/apt/archives/lock
+sudo rm -f /var/lib/dpkg/lock*
+sudo dpkg --configure -a
+set -e
+APP_DIR=${1:-$HOME}
+sudo apt-get install -y git
+
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,17 +9,29 @@ terraform {
 
 
 provider "yandex" {
-  token = "y0_AgAAAABzhbGEAATuwQAAAAD5QksDAAAiJEL2WFhEUL-FfsuB5hnIL0T1pg"
-  cloud_id = "b1gf7tfgqmq6e1sqa4u4"
-  folder_id = "b1g36pr10f95b2irvqck"
-  zone = "ru-central1-a"
+  service_account_key_file = var.service_account_key_file
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
+  zone = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
+  platform_id = "standard-v2"
+  zone = var.instance_app_zone
+
   resources {
     cores = 2
     memory = 2
+}
+
+  connection {
+    type = "ssh"
+    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user = "ubuntu"
+    agent = false
+# путь до приватного ключа
+    private_key = file(var.private_key_path)
 }
 
   provisioner "file" {
@@ -27,17 +39,27 @@ resource "yandex_compute_instance" "app" {
     destination = "/tmp/puma.service"
 }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/appuser.pub")}"
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
 }
+
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+}
+
   boot_disk {
     initialize_params {
 # Указать id образа созданного в предыдущем домашнем задании
-   image_id = "fd85jf27ie4dcc6232mv"}
+   image_id = var.image_id
+}
+
+
 }
 
   network_interface {
 # Указан id подсети default-ru-central1-a
-    subnet_id = "e9bhkneprjbvqmc7sgof"
-    nat = true}
+    subnet_id = var.subnet_id
+    nat = true
+}
 }
