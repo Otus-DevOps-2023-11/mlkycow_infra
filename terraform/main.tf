370c6f0c6279c502feca49002257ead1e803ac47
mlkycow <vileninuljanov@yandex.ru>
2024-02-15 02:34:46 +0300

    Update README.md & terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,56 +10,56 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id = var.cloud_id
-  folder_id = var.folder_id
-  zone = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
+  name        = "reddit-app"
   platform_id = "standard-v2"
-  zone = var.instance_app_zone
+  zone        = var.instance_app_zone
 
   resources {
-    cores = 2
+    cores  = 2
     memory = 2
-}
+  }
 
   connection {
-    type = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user = "ubuntu"
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
     agent = false
-# путь до приватного ключа
+    # путь до приватного ключа
     private_key = file(var.private_key_path)
-}
+  }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
-}
+  }
 
   provisioner "remote-exec" {
     script = "files/deploy.sh"
-}
+  }
 
 
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
-}
+  }
 
   boot_disk {
     initialize_params {
-# Указать id образа созданного в предыдущем домашнем задании
-   image_id = var.image_id
-}
+      # Указать id образа созданного в предыдущем домашнем задании
+      image_id = var.image_id
+    }
 
 
-}
+  }
 
   network_interface {
-# Указан id подсети default-ru-central1-a
+    # Указан id подсети default-ru-central1-a
     subnet_id = var.subnet_id
-    nat = true
-}
+    nat       = true
+  }
 }
