ea0216e61887a3a77777b4db1b70b039441def98
mlkycow <vileninuljanov@yandex.ru>
2024-02-18 10:48:40 +0300

    Newbranch terraform-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,17 @@ provider "yandex" {
   zone                     = var.zone
 }
 
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "reddit-app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.app-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
 resource "yandex_compute_instance" "app" {
   name        = "reddit-app"
   platform_id = "standard-v2"
@@ -54,6 +65,10 @@ resource "yandex_compute_instance" "app" {
       image_id = var.image_id
     }
 
+    network_interface {
+      subnet_id = yandex_vpc_subnet.app-subnet.id
+      nat       = true
+    }
 
   }
 
